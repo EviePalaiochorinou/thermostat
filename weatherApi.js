72e2b93f6c9f06244420dae58d429e79e040def5
Evie <evangeliapal@gmail.com>
2022-03-28 20:17:46 +0100

    Fetch weather temperature for any city

---
+++ weatherApi.js
@@ -0,0 +1,26 @@
+import { got } from 'got';
+import dotenv from 'dotenv';
+dotenv.config();
+
+class WeatherApi {
+  constructor() {
+    this.apiKey = `${process.env.API_KEY}`
+  }
+
+
+  fetchWeatherData(city, callback) {
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${this.apiKey}`;
+    let weatherData = null;
+    got(apiUrl).then((response) => {
+      weatherData = JSON.parse(response.body);
+      callback(weatherData.main.temp);
+    });
+  }
+
+};
+
+export { WeatherApi };
+const weather = new WeatherApi();
+weather.fetchWeatherData('London', (weatherResponse) => {
+  console.log(weatherResponse);
+});  
