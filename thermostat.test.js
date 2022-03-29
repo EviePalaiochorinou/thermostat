4cf2516bf57fe22f26d5c511557269911318a429
Evie <evangeliapal@gmail.com>
2022-03-29 19:52:50 +0100

    Test the thermostat class's functions

--- thermostat.test.js
+++ thermostat.test.js
@@ -5,4 +5,48 @@ describe('Thermostat', () => {
         const thermostat = new Thermostat();
         expect(thermostat.getTemperature()).toEqual(20);
     });
-})
+    it('increases the temperature by 1', () => {
+        const thermostat = new Thermostat();
+        thermostat.up();
+        expect(thermostat.temperature).toEqual(21);
+      });
+      it('reduces the temperature by 1', () => {
+        const thermostat = new Thermostat();
+        thermostat.down();
+        expect(thermostat.temperature).toEqual(19);
+      });
+      it('cannot be lower than 10', () => {
+        const thermostat = new Thermostat();
+        for (let i = 0 ; i < 11 ; i++) {
+        thermostat.down();
+        }
+        expect(thermostat.temperature).toEqual(10);
+      });
+      it('turns the power saving mode off', () => {
+          const thermostat = new Thermostat();
+          thermostat.powerSavingToggle
+          expect(thermostat.powerSaveMode).toEqual(false);
+      });
+      it('changes max temp to 25 if is on', () => {
+        const thermostat = new Thermostat();
+        thermostat.powerSavingToggle;
+        expect(thermostat.maxTemp).toEqual(25);
+      });
+      it('resets the temperature', () => {
+        const thermostat = new Thermostat();
+        for (let i = 0 ; i < 5 ; i++) {
+        thermostat.down();
+        }
+        thermostat.reset();
+        expect(thermostat.temperature).toEqual(20);
+      });
+      it('returns energy usage is low', () => {
+        const thermostat = new Thermostat();
+        for (let i = 0 ; i < 5 ; i++) {
+        thermostat.down();
+        }
+        expect(thermostat.getEnergyUsage()).toEqual('low-usage');
+      });
+      //mock the weather class
+      //test the 'setCity' function
+});
